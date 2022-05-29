57352944cc04cdf36ac0e23dab0ab58ba222e71f
milliwonkim <rldnjs9347@gmail.com>
2022-05-30 00:19:27 +0900

    [CHORE] 선별적 테스트 추가 3

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -2,7 +2,7 @@
 
 describe("My First Test", () => {
   it("꼭 필요한 테스트", { tags: "mandatory" }, () => {
-    cy.visit("http://localhost:3000/landing")
+    cy.visit("/landing")
     cy.contains("h1", "You did it!")
     cy.intercept("GET", "/dev/getLambda", {
       hostname: "https://zkhru142hl.execute-api.ap-northeast-2.amazonaws.com",
@@ -15,7 +15,7 @@ describe("My First Test", () => {
   })
 
   it("필요 없는 테스트", () => {
-    cy.visit("http://localhost:3000/landing")
+    cy.visit("/landing")
     cy.contains("h1", "You did it!")
     cy.intercept("GET", "/dev/getLambda", {
       hostname: "https://zkhru142hl.execute-api.ap-northeast-2.amazonaws.com",
